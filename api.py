cc735e2bb3c91b709908e3cabfc4bb9afcc3347e
Srinickethan <srinick@terpmail.umd.edu>
2023-12-15 14:13:45 -0500

    Updated ML model file and API

--- api.py
+++ api.py
@@ -7,16 +7,25 @@ st.title("Prediction of hospital admission")
 st.write("##### This model helps in predicting the percentage of a patient's likelihood that he would be admitted into the hospital based on various factors and the hospital's amenities avilable")
 
 st.sidebar.write("### Enter the inputs")
-angle = st.sidebar.number_input("Hemoglobin (in g/dl)",1.0,359.9)
-thickness = st.sidebar.number_input("Glucose (in mmol/L)",0.0,25.0)
-vType = st.sidebar.number_input("Raised cardiac enzyme (in boolean)",1.0,200.0)
-pRadius = st.sidebar.number_input("Heart disease (in boolean)",0.0,100.0)
+AGE = st.sidebar.number_input("Age (in number)",1.0,100.0)
+HB= st.sidebar.number_input("Heamoglobin level (in g/DL)",5.0,25.0)
+GLUCOSE= st.sidebar.number_input("Glucose (in mg/DL)",50.0,2700.0)
+
+options = ['0', '1']
+GENDER = st.radio("Gender (1 if male, 0 if female)", options)
+RURAL = st.radio("Rural (1 if rural, 0 if urban)", options)
+SMOKING = st.radio("Smoking (1 if smoker, 0 ifnon-smoker)", options)
+ALCOHOL = st.radio("Alcohol (1 if you consume alcohol, 0 if non-alcoholic)", options)
+DM = st.radio(" Diabetes Mellitus (1 if diabetic, 0 if non-diabetic)", options)
+HTN = st.radio("Hypertension (1 if hypersion, 0 if non-hypertension)", options)
+RAISED_CARDIAC_ENZYMES = st.radio(" Raise cardiac enzymes (1 if RCM Syomptomatic, 0 if non-RCM Symptomatic)", options)
+
 
 submit = st.sidebar.button('Calculate')
 
 if submit:
-    model = joblib.load('HDHI Admission Data.csv')
-    input = [[Hemoglobin, Glucose, Raised cardiac enzyme, Heart disease]]
+    model = joblib.load('model.pkl')
+    input = [[AGE,GENDER, RURAL, SMOKING, ALCOHOL, DM, HTN, HB, GLUCOSE, RAISED_CARDIAC_ENZYMES]]
     Hospital = model.predict(input)
     st.write(" ## Percentage prediction : {:0.2f}".format(Hospital[0]))
 else:
--- final_project.py
+++ final_project.py
@@ -1,64 +1,28 @@
-# Import necessary libraries
 import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.impute import SimpleImputer
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report
-from flask import Flask, request, jsonify
+from sklearn.ensemble import RandomForestRegressor
+import sklearn.metrics as metrics
+import joblib
 
+data = pd.read_csv('cleaned_HDHI_Admission_data.csv')
+data = data.dropna()
+x = data[['HB', 'Glucose', 'RAISED CARDIAC ENZYMES', 'HEART FAILURE']].to_numpy()
+y = data['Output'].to_numpy()
+print(x.shape, y.shape)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=37)
+print(x_train.shape, y_train.shape)
+print(x_test.shape, y_test.shape)
 
-# Step 1: Data Gathering
-# Read the hospital admission dataset from a file (e.g., CSV)
-data = pd.read_csv("hospital_admission_data.csv")
+model = RandomForestRegressor(n_estimators = 100, random_state = 0)
+model.fit(x_train, y_train)
 
-# Step 2: Data Preprocessing
-# Handle missing values
-imputer = SimpleImputer(strategy="mean")
-data = imputer.fit_transform(data)
+def evaluate(y_train, y_test):
+    y_pred = model.predict(x_train)
+    train_rmse = (metrics.mean_squared_error(y_train, y_pred, squared=False))
+    print("Training RMSE: ",train_rmse)
+    y_pred = model.predict(x_test)
+    test_rmse = (metrics.mean_squared_error(y_test, y_pred, squared=False))
+    print("Testing RMSE: ",test_rmse)
 
-# Handle outliers
-# Perform data cleaning, feature scaling, etc.
-
-# Step 3: Feature Selection and Engineering
-# Identify and engineer the most informative features
-# Domain-specific feature selection and engineering code
-
-# Step 4: Machine Learning Models
-# Split the data into training and testing sets
-X = data.drop("admission_status", axis=1)
-y = data["admission_status"]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Choose and train predictive models
-model = RandomForestClassifier()
-model.fit(X_train, y_train)
-
-# Step 5: Model Training
-
-# Step 6: Performance Evaluation
-# Make predictions on the test set
-y_pred = model.predict(X_test)
-
-# Evaluate model performance
-accuracy = accuracy_score(y_test, y_pred)
-report = classification_report(y_test, y_pred)
-
-# Step 7: User Interface
-# Create a Flask web application for the user interface
-app = Flask(__name)
-
-@app.route('/predict_admission', methods=['POST'])
-def predict_admission():
-    # Receive patient information from the user
-    patient_data = request.json
-    
-    # Preprocess and predict using the trained model
-    prediction = model.predict(patient_data)
-    
-    # Return the prediction to the user
-    return jsonify({"prediction": prediction})
-
-if __name__ == '__main__':
-    app.run(debug=True)
+evaluate(y_train, y_test)
+joblib.dump(model, 'model.pkl') 
